c01b73c6670dedb5672d8755f87eda6afee072a7
Tapokera Nkhonjera <tapotandane@gmail.com>
2025-05-27 16:43:53 +0200

    person class

--- SchoolSystem/src/main/java/Person/Person.java
+++ SchoolSystem/src/main/java/Person/Person.java
@@ -9,5 +9,24 @@ package Person;
  * @author stati
  */
 public class Person {
-    
+    // Attributes
+    protected String name;
+    protected int age;
+    protected String gender;
+
+    // Constructor
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    // Method to display details
+    public void displayDetails() {
+        System.out.println(" Person Details ");
+        System.out.println("Name   : " + name);
+        System.out.println("Age    : " + age);
+        System.out.println("Gender : " + gender);
+    }
 }
+
