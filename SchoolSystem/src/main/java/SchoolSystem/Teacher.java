9349ec2fdfc436cb227def99c0c5d0886e17c3e7
washingtonchilima <chilimawashington20@gmail.com>
2025-05-27 10:46:09 -0500

    Implemented Teacher class that extends Person.

--- SchoolSystem/src/main/java/SchoolSystem/Teacher.java
+++ SchoolSystem/src/main/java/SchoolSystem/Teacher.java
@@ -2,12 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+
 package SchoolSystem;
 
-/**
- *
- * @author stati
- */
-public class Teacher {
-    
+public class Teacher extends Person {
+    private final String employeeId;
+    private final String subject;
+
+    public Teacher(String name, int age, String gender, String employeeId, String subject) {
+        super(name, age, gender);
+        this.employeeId = employeeId;
+        this.subject = subject;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Subject: " + subject);
+    }
 }
