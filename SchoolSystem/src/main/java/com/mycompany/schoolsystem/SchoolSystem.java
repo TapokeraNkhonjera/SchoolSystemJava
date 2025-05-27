57bdc9dce70c4067a9b5e452e705018b59193141
Tapokera Nkhonjera <tapotandane@gmail.com>
2025-05-27 15:53:19 +0200

    Initial skeleton with base classes

---
+++ SchoolSystem/src/main/java/com/mycompany/schoolsystem/SchoolSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.schoolsystem;
+
+/**
+ *
+ * @author stati
+ */
+public class SchoolSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
