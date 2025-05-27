37749e75e45f4ceb15a3d05d9bd8e3ff46f93f96
USER <USER@Nancy03>
2025-05-27 17:19:45 +0200

    fixed student error and added implemented staff class code

--- SchoolSystem/src/main/java/SchoolSystem/Staff.java
+++ SchoolSystem/src/main/java/SchoolSystem/Staff.java
@@ -8,6 +8,22 @@ package SchoolSystem;
  *
  * @author stati
  */
-public class Staff {
-    
+public class Staff extends Person {
+
+    private String staffId;
+    private String department;
+
+    public Staff(String name, int age, String gender, String staffId, String department) {
+        super(name, age, gender);
+        this.staffId = staffId;
+        this.department = department;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Staff ID: " + staffId);
+        System.out.println("Department: " + department);
+        System.out.println();
+    }
 }
